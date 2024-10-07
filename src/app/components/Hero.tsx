4743e2c3996b29f0d7b295d680cca662dd774a6d
Tarik T <57813046+devwankel@users.noreply.github.com>
2024-10-07 19:02:48 +0300

    fix: Links

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
                 <h2 className="text-5xl font-medium">Maximum Returns in</h2>
                 <h2 className="text-5xl font-medium">DeFi with Minimum Effort</h2>
                 <h3 className="text-xl mt-3">Grow Your Assets Smartly with SoneYield</h3>
-                <button className="text-gray-600 font-thin bg-button hover:bg-layer hover:text-text custom-border duration-500 ease-in-out px-4 py-2 rounded-lg mt-3">Launch Dapp</button>
+                <Link href={'https://alpha-v1.soneyield.com/'}><button className="text-gray-600 font-thin bg-button hover:bg-layer hover:text-text custom-border duration-500 ease-in-out px-4 py-2 rounded-lg mt-3">Launch Dapp</button></Link>
               </div>
               <div className="flex gap-3 mx-auto items-center">
                 <Link href={'#'}><FontAwesomeIcon className="w-8 hover:text-button duration-500 ease-in-out" icon={faTwitter}/></Link>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -30,9 +30,11 @@ const Navbar = () => {
           </ul>
         </nav>
 
-        <button className="text-gray-600 font-thin bg-button hover:bg-layer hover:text-text custom-border duration-500 ease-in-out px-4 py-2 rounded-lg">
-          Launch Dapp
-        </button>
+        <Link href={'https://alpha-v1.soneyield.com/'}>
+          <button className="text-gray-600 font-thin bg-button hover:bg-layer hover:text-text custom-border duration-500 ease-in-out px-4 py-2 rounded-lg">
+            Launch Dapp
+          </button>
+        </Link>
       </div>
     </div>
   );
